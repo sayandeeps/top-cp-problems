4c23e4e97eb938081d9716802970417266c3238a
Sayandeep <sayandeep11@gmail.com>
2023-01-24 12:22:29 +0530

    easiest solution for pasha and stick

---
+++ pasha_and_stick.cpp
@@ -0,0 +1,16 @@
+//pasha and stick
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	long long int n;
+	cin>>n;
+	if(n%2!=0){
+		cout<<0;
+	}
+	else if((n/2)%2!=0){
+		cout<<(n/2)/2;
+	}
+	else if((n/2)%2==0){
+		cout<<((n/2)/2)-1;
+	}
+}
